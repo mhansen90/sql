ce8b25c0a07775a271bfbaa716166191741c9986
Meagan Hansen <mhansengrutz@gmail.com>
2018-10-27 16:53:33 -0400

    addedcostumertables

---
+++ Customer_Queries.sql
@@ -0,0 +1,14 @@
+select * from customer;
+
+select * from customer
+where state = 'CA'
+order by name DESC;
+
+select Name, City, State, Sales, Active as 'Is Active?' from customer
+where state = 'CA'
+order by name DESC;
+
+select * from customer
+where state = 'CA'
+ and city = 'Fresno'
+order by name DESC;
