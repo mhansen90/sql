f0c940dda416c02aa7d0614efc81aebf80a5367b
Meagan Hansen <mhansengrutz@gmail.com>
2018-10-27 15:09:38 -0400

    CloneMeSQL

---
+++ Day1.sql
@@ -0,0 +1,17 @@
+select * from product
+where id = 4;
+select * from product
+where ListPrice <55;
+select * 
+from product 
+order by Code;
+select * 
+from product	
+order by ListPrice;
+INSERT INTO Product
+Values ('fun', 'Murach''s Fundamentals', '17.00');
+select * from Product
+order by ID;
+INSERT into Product
+Values ('9', 'fun', 'Murach''s Fundamentals', '10.00')
+Values ('10', 'his', 'Murach''s Hist);
